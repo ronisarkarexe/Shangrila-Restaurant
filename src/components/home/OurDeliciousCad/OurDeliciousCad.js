f65a7c0b8bc4d9e7646e44be5fe36b9f10cb7701
swapno <swapno5896@yahoo.com>
2021-05-30 08:17:29 -0700

    d

---
+++ src/components/home/OurDeliciousCad/OurDeliciousCad.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const OurDeliciousCad = (props) => {
+  console.log(props);
+  const { title, describtion, img } = props.dt;
+  return (
+    <>
+      <div class="card mb-3" style={{ maxWidth: "540px" }}>
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img style={{ width: "166px" }} src={img} alt="..." />
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">{title}</h5>
+              <p class="card-text">{describtion}</p>
+              <p class="card-text">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default OurDeliciousCad;
--- src/mainComponent/MainComponent.js
+++ src/mainComponent/MainComponent.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Blogs from '../components/home/blogs/Blogs';
 import Navbar from '../components/home/Navbar/Navbar';
+import OurDelicious from '../components/home/ourDelicious/OurDelicious';
 
 const MainComponent = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Blogs></Blogs>
+            <OurDelicious></OurDelicious>
         </div>
     );
 };

28ce8256e92b17a6a0f92422a40e5a3b196dc9ea
swapno <swapno5896@yahoo.com>
2021-05-31 01:59:20 -0700

    d

--- src/mainComponent/MainComponent.js
+++ src/mainComponent/MainComponent.js
@@ -1,26 +1,19 @@
 import React from 'react';
-<<<<<<< HEAD
-import Blogs from '../components/home/Blogs/Blogs';
+
 import Footer from '../components/home/Footer/Footer';
 import Gallery from '../components/home/Gallery/Gallery';
-=======
 import Blogs from '../components/home/blogs/Blogs';
-import Navbar from '../components/home/Navbar/Navbar';
 import OurDelicious from '../components/home/ourDelicious/OurDelicious';
 import Shef from '../components/home/Shef/Shef';
->>>>>>> 79c43d74185900ca2b6724d88b6762968ce08263
 
 const MainComponent = () => {
     return (
         <div>
             <Gallery></Gallery>
             <Blogs></Blogs>
-<<<<<<< HEAD
             <Footer></Footer>
-=======
             <OurDelicious></OurDelicious>
             <Shef></Shef>
->>>>>>> 79c43d74185900ca2b6724d88b6762968ce08263
         </div>
     );
 };
